ec35effae2b863e9abecc0acbeec91a57bb23d9e
Anton Semjonov <anton@semjonov.de>
2020-04-01 23:39:16 +0200

    fix a bug in i2c clock script: hexdump squeezing

--- firmware/i2c-clock.sh
+++ firmware/i2c-clock.sh
@@ -6,11 +6,11 @@ DEV="0x20" # configured device address of vfd
 while true; do
 
   # format HH:MM string
-  HHMM=$(printf "%s" "$(date +%H%M)" | hexdump -e '/1 "0x%02x "')
-  SECS=$([[ $(( "$(date +%-S)" % 2 )) -eq 0 ]] && echo 0x3a || echo 0x00)
+  HHMM="$(printf "%s" "$(date "+%H%M")" | hexdump -ve '/1 "0x%02x "')"
+  SECS="$([[ $(( "$(date "+%-S")" % 2 )) -eq 0 ]] && echo "0x3a" || echo "0x00")"
   
   # transfer text to vfd buffer
-  i2ctransfer -y $BUS w6@$DEV 0 $HHMM $SECS
+  i2ctransfer -y "$BUS" "w6@$DEV" 0 $HHMM "$SECS"
   sleep 0.1
 
 done
