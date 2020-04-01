1b8c1abd132b8a792f189ef2b547890eeb56e680
Anton Semjonov <anton@semjonov.de>
2020-04-02 00:34:15 +0200

    add aux script to set rtc date via pi2co dongle

---
+++ aux/set-current-rtc-date.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+# set the current date in a DS3231 RTC via i2c
+BUS=10
+DEV="0x68"
+
+# print before values
+echo    "        sec  min  hr   wkd  day  mon  year"
+echo -n "before: "
+i2ctransfer -y "$BUS" "w1@$DEV" 0 r7
+
+# write current date
+i2ctransfer -y "$BUS" "w8@$DEV" 0 $(date "+0x%S 0x%M 0x%H 0x%02u 0x%d 0x%m 0x%y")
+
+# print after values
+echo -n "after:  "
+i2ctransfer -y "$BUS" "w1@$DEV" 0 r7
